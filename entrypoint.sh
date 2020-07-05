4f0ef8a78430fb6091f7f78fd54662a764832b74
Yuki Kikuchi <bclswl0827@foxmail.com>
2020-07-05 17:25:53 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,75 @@
 #!/bin/bash
-sed -i "s/NULL/$DEVICE/g" /etc/goestools/goesrecv.conf
+
+if [ -z "$DEVICE" ]; then
+	echo >&2 'error: missing required DEVICE environment variable'
+	echo >&2 '  Did you forget to -e DEVICE=... ?'
+	exit 1
+fi
+
+if [ -z "$GAIN" ]; then
+	echo >&2 'error: missing required GAIN environment variable'
+	echo >&2 '  Did you forget to -e GAIN=... ?'
+	exit 1
+fi
+
+if [ $DEVICE = "airspy" ] ; then
+cat << EOF > /etc/goestools/goesrecv.conf
+[demodulator]
+satellite = "GK-2A"
+downlink = "lrit"
+source = "airspy"
+[airspy]
+frequency = 1692140000
+gain = $GAIN
+[costas]
+max_deviation = 200e3
+[clock_recovery.sample_publisher]
+bind = "tcp://0.0.0.0:5002"
+send_buffer = 2097152
+[quantization.soft_bit_publisher]
+bind = "tcp://0.0.0.0:5001"
+send_buffer = 1048576
+[decoder.packet_publisher]
+bind = "tcp://0.0.0.0:5004"
+send_buffer = 1048576
+[demodulator.stats_publisher]
+bind = "tcp://0.0.0.0:6001"
+[decoder.stats_publisher]
+bind = "tcp://0.0.0.0:6002"
+EOF
+elif
+	[ $DEVICE = "rtlsdr" ] ; then
+cat << EOF > /etc/goestools/goesrecv.conf
+[demodulator]
+satellite = "GK-2A"
+downlink = "lrit"
+source = "rtlsdr"
+[rtlsdr]
+frequency = 1692140000
+sample_rate = 1024000
+gain = $GAIN
+[costas]
+max_deviation = 200e3
+[clock_recovery.sample_publisher]
+bind = "tcp://0.0.0.0:5002"
+send_buffer = 2097152
+[quantization.soft_bit_publisher]
+bind = "tcp://0.0.0.0:5001"
+send_buffer = 1048576
+[decoder.packet_publisher]
+bind = "tcp://0.0.0.0:5004"
+send_buffer = 1048576
+[demodulator.stats_publisher]
+bind = "tcp://0.0.0.0:6001"
+[decoder.stats_publisher]
+bind = "tcp://0.0.0.0:6002"
+EOF
+else
+	echo >&2 'error: incorrect DEVICE environment variable'
+	echo >&2 '  Check your DEVICE string and deploy again?'
+	exit 1
+fi
+
 goesrecv -i 1 -c /etc/goestools/goesrecv.conf &
 cd /xrit-rx/src
 python3 xrit-rx.py
